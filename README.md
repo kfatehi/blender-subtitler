74b3711d3866aebf5f7455d53afee7547708bddb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-12-03 22:23:52 -0800

    make it work right

--- README.md
+++ README.md
@@ -2,18 +2,12 @@
 
 super simple subtitle script for blender (last used on blender v2.78)
 
-reads an internal text file containing a very simple format for subtitles, example:
+reads text file containing a very simple format for subtitles, example:
 
 ```
-82:161 Hi, how are you?
-298:332 I'm good, it's my birthday today.
-516:560 Great I'm happy to hear that!
+82-161 Hi, how are you?
+298-332 I'm good, it's my birthday today.
+516-560 Great I'm happy to hear that!
 ```
 
-the script will manage a Text strip on your sequencer. you must create this and name it "Subtitles"
-
-at frame 0, the script resets its internal state, which is to load the first subtitle
-
-at each frame, the decision is made to show/hide the subtitle or advance to the next one
-
-that's all i needed! hope it saves you time.
+the script will create text strips for each of these
--- subtitler.py
+++ subtitler.py
@@ -1,60 +1,29 @@
 import bpy
 import re
 
-SUB_LIST = []
-
-subPattern = re.compile("^(\d+):(\d+) (.+)$")
-rawSubs = bpy.data.texts['subs.txt'].as_string()
-for line in rawSubs.split("\n"):
-    (start, end, txt) = re.search(subPattern, line).groups()
-    SUB_LIST.append((int(start), int(end), txt))
-
-subtitleTrack = bpy.data.scenes['Scene'].sequence_editor.sequences_all['Subtitles']
-
-def setSubtitles(text):
-    subtitleTrack.text = text
-
-def clearSubtitles():
-    setSubtitles("");
+class Subtitler():
+    def __init__(self):
+        subPattern = re.compile("^(\d+)-(\d+) (.+)$")
+        self.SUB_LIST = []
+        rawSubs = bpy.data.texts['subs.txt'].as_string()
+        for line in rawSubs.split("\n"):
+            try:
+                (start, end, txt) = re.search(subPattern, line).groups()
+                self.SUB_LIST.append((int(start), int(end), txt))
+            except AttributeError:
+                pass
+            
+    def getSubtitlesList(self):
+        return self.SUB_LIST
     
+    def addStripsToChannel(self, ch):
+        scene = bpy.data.scenes['Scene']
+        seqs = scene.sequence_editor.sequences
+        for (start, end, txt) in self.getSubtitlesList():
+            name = "sub"+str(start)
+            strip = seqs.new_effect(name, "TEXT", ch, start, end)
+            strip.text = txt
+            strip.wrap_width = 1
+            strip.blend_type = "ADD"
 
-subCount = len(SUB_LIST)
-
-pos = 0
-sub = SUB_LIST[pos]
-done = False
-
-def next():
-    global pos, sub, done
-    pos += 1
-    if pos == subCount:
-        sub = None
-        done = True
-    else:
-        sub = SUB_LIST[pos]
-    
-    
-def my_handler(scene):
-    frame = scene.frame_current
-    print (done, sub, pos, frame)
-    if frame == 0:
-        global pos, sub, done
-        pos = 0
-        sub = SUB_LIST[pos]
-        done = False
-    if done:
-        return None
-    if frame < sub[0]:
-        return clearSubtitles();
-    if frame >= sub[0] and frame <= sub[1]:
-        return setSubtitles(sub[2])
-    if frame > sub[1]:
-        next()
-        return clearSubtitles();
-
-# https://www.blender.org/api/blender_python_api_2_63_release/bpy.app.handlers.html
-bpy.app.handlers.frame_change_pre.clear()
-print("Cleared frame_change_pre handlers")
-if len(SUB_LIST) > 0:
-    print("Registered subtitle handler")
-    bpy.app.handlers.frame_change_pre.append(my_handler)
+Subtitler().addStripsToChannel(3)
